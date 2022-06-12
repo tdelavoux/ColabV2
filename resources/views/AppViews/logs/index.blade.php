41d70cbcd95007d4d4c961ecf306d935e2b148fd
Thibaut <thibault.delavoux@gmail.com>
2022-06-12 12:31:49 +0200

    Fet. Front : Mookup Logs

---
+++ resources/views/AppViews/logs/index.blade.php
@@ -0,0 +1,221 @@
+<div class="log-content">
+
+
+    <div class="log-filter">
+        <div class="a-input-group m-0 mr-1">
+            <span role="button" class="a-input-button"><i class="fas fa-search"></i></span>
+            <input id="side-search" class="a-input a-borderfull" placeholder="Search">
+        </div>
+
+        <button class="a-btn a-light"><i class="fa-solid fa-filter"></i> Filters</button>
+    </div>
+
+
+    <h3>Yesterday</h3>
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault2.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Jean Registre</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault3.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Lara Clure</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div>
+            <div class="log-line">
+                <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault.jpg') }}');"></div>
+                <div class="log-line-args">
+                    <span class="log-line-name">Thibault Delavoux</span>
+                    <span class="log-line-text">has commented the task</span>
+                    <span class="log-line-item">#1543 - UI/Mookup Logs page</span>
+                </div>
+            </div>
+            
+            <div class="log-line-commented">
+                <div class="comment-leither">
+                </div>
+            
+                <div class="log-line-comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi delectus voluptatibus ipsam laboriosam consectetur enim. Nisi repellat reprehenderit eligendi. Et, doloribus inventore reprehenderit fuga iusto quasi. Incidunt voluptas ex hic.
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault10.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Alex Ception</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+
+    <h3>Last Week</h3>
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault5.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Sandra Nicouette</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault9.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Jean File</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div>
+            <div class="log-line">
+                <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault.jpg') }}');"></div>
+                <div class="log-line-args">
+                    <span class="log-line-name">Thibault Delavoux</span>
+                    <span class="log-line-text">has commented the task</span>
+                    <span class="log-line-item">#1543 - UI/Mookup Logs page</span>
+                </div>
+            </div>
+            
+            <div class="log-line-commented">
+                <div class="comment-leither">
+                </div>
+            
+                <div class="log-line-comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi delectus voluptatibus ipsam laboriosam consectetur enim. Nisi repellat reprehenderit eligendi. Et, doloribus inventore reprehenderit fuga iusto quasi. Incidunt voluptas ex hic.
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="log-row">
+
+        <div class="log-left">
+            <div role="button" class="a-round-img-sm" style="background-image:url('{{ asset('img/slack.png') }}');"></div>
+            <span class="log-left-leither"></span>
+        </div>
+
+        <div class="log-date">
+            24/06/2022
+        </div>
+
+        <div class="log-line">
+            <div role="button" class="a-round-img-sm user-img" style="background-image:url('{{ asset('img/userDefault.jpg') }}');"></div>
+            <div class="log-line-args">
+                <span class="log-line-name">Thibault Delavoux</span>
+                <span class="log-line-text">has created the board</span>
+                <span class="log-line-item">#Colab Web Dev</span>
+            </div>
+        </div>
+
+    </div>
+    
+
+</div>
