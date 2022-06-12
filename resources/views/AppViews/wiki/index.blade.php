c0d472a12897316d423435abf030725aaad3d86f
Thibaut <thibault.delavoux@gmail.com>
2022-06-12 12:30:44 +0200

    Fet. Front : Mookup Wiki

---
+++ resources/views/AppViews/wiki/index.blade.php
@@ -0,0 +1,42 @@
+<div class="wiki-content">
+
+    @include('AppViews.wiki.partials.wiki_nav')
+
+    <div class="wiki-page">
+        <h2>Getting Started</h2>
+        <h3>Requirements</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aliquam nesciunt fugiat, illo molestias repellat tempore unde delectus molestiae eum omnis est dolorem culpa, in incidunt laboriosam explicabo sunt nisi?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dicta doloremque quae numquam quam sint eos quis, sapiente eius reiciendis enim, ipsum, tenetur expedita cumque? Nam velit sequi at soluta?</p>
+        <pre class="brush: html">
+            <div>       
+                <span id="collapserDemo" collapse-target="#toCollapseDemo" class="small-text">Collapse below</span>
+                <p id="toCollapseDemo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate non nisi id tempor. Sed lobortis ligula sed 
+                    erat bibendum, vel interdum dui tempor. Etiam faucibus dapibus lectus vel rutrum. In vel neque malesuada, 
+                    tristique nibh quis, porttitor dui. Nullam turpis erat, luctus sed nibh at, mollis rhoncus neque. Aenean eleifend dui nisi. 
+                    Donec iaculis pretium porta. Donec at orci iaculis, lobortis nulla in, hendrerit lorem. Mauris nunc ipsum, eleifend id posuere eget, 
+                    viverra non nisi.
+                </p>
+            </div>
+        </pre>
+        <p>Make sure you read documentation</p>
+        
+        <div class="wiki-separator"></div>
+
+        <h3>Requirements</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aliquam nesciunt fugiat, illo molestias repellat tempore unde delectus molestiae eum omnis est dolorem culpa, in incidunt laboriosam explicabo sunt nisi?</p>
+        
+        <pre class="brush: html">
+            <div>       
+                <span id="collapserDemo" collapse-target="#toCollapseDemo" class="small-text">Collapse below</span>
+                <p id="toCollapseDemo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate non nisi id tempor. Sed lobortis ligula sed 
+                    erat bibendum, vel interdum dui tempor. Etiam faucibus dapibus lectus vel rutrum. In vel neque malesuada, 
+                    tristique nibh quis, porttitor dui. Nullam turpis erat, luctus sed nibh at, mollis rhoncus neque. Aenean eleifend dui nisi. 
+                    Donec iaculis pretium porta. Donec at orci iaculis, lobortis nulla in, hendrerit lorem. Mauris nunc ipsum, eleifend id posuere eget, 
+                    viverra non nisi.
+                </p>
+            </div>
+        </pre>
+        <p>Make sure you read documentation</p>
+    </div>
+
+</div>
---
+++ resources/views/AppViews/wiki/partials/wiki_nav.blade.php
@@ -0,0 +1,36 @@
+<div class="wiki-nav p-2">
+    <div class="a-input-group mh-1 w80 mb-1">
+        <span role="button" class="a-input-button"><i class="fas fa-search"></i></span>
+        <input id="side-search" class="a-input a-borderfull" placeholder="Search">
+    </div>
+
+    <div class="sidenav-content">
+        <span role="button" class="nav-link-left wiki-nav-link a-collapsable" collapse-target="#toCollapseDemo"><span>Introduction</span><i class="fa-solid fa-angle-down"></i></span>
+        <ul id="toCollapseDemo" class="a-collapsed">
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Overview</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Architecture</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Glossary</span></i></a></li>
+        </ul>
+
+        <span role="button" class="nav-link-left wiki-nav-link a-collapsable" collapse-target="#toCollapseDemo2"><span>Getting Started</span><i class="fa-solid fa-angle-down"></i></span>
+        <ul id="toCollapseDemo2" class="a-collapsed">
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Overview</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link active" href="#"><span>Architecture</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Glossary</span></i></a></li>
+        </ul>
+        
+        <span role="button" class="nav-link-left wiki-nav-link a-collapsable" collapse-target="#toCollapseDemo3"><span>Deployement</span><i class="fa-solid fa-angle-down"></i></span>
+        <ul id="toCollapseDemo3" class="a-collapsed">
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Overview</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Architecture</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Glossary</span></i></a></li>
+        </ul>
+
+        <span role="button" class="nav-link-left wiki-nav-link a-collapsable" collapse-target="#toCollapseDemo4"><span>Release note</span><i class="fa-solid fa-angle-down"></i></span>
+        <ul id="toCollapseDemo4" class="a-collapsed">
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Overview</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Architecture</span></a></li>
+            <li><a class="nav-link-left wiki-nav-sub-link" href="#"><span>Glossary</span></i></a></li>
+        </ul>
+    </div>
+</div>
