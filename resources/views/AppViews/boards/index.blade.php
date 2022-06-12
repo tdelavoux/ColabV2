d78ee09d94076bc37aa3baca7a28a0a60df0f47d
Thibaut <thibault.delavoux@gmail.com>
2022-06-12 12:29:12 +0200

    Fet. Front : Mookup Boards

---
+++ resources/views/AppViews/boards/index.blade.php
@@ -0,0 +1,72 @@
+<div class="p-2">
+    @include('AppViews.project.timeline ')
+
+    <div class="board-row">
+        <div class="board-row mt-2 mh-05">
+            <div class="board-card">
+                <div class="board-card-header">
+                    <span>Backend Developpment</span>
+                    <span class="progress-bar">
+                        <progress class="progress primary" max="100" value="70"></progress><span>70%</span>
+                    </span>
+                    
+                </div>
+    
+                <div class="board-card-content-tags mb-1">
+                    <span class="kanban-tag tag-r">Developpment</span>
+                    <span class="kanban-tag tag-y">Research</span>
+                    <span class="kanban-tag">Planing</span>
+                </div>
+    
+                <div class="board-card-footer">
+                    <div class="board-card-items">
+                        <span class="board-card-item"><i class="fa-solid fa-table-list"></i> 51</span>
+                        <span class="board-card-item"><i class="fa-brands fa-trello"></i> 25</span>
+                        <span class="board-card-item"><i class="fa-solid fa-bug"></i> 12</span>
+                        <span class="board-card-item"><i class="fa-solid fa-mug-hot"></i> 100+</span>
+                    </div>
+                    <div class="kanban-users-strack">
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault2.jpg') }}');"></div>
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault9.jpg') }}');"></div>
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault.jpg') }}');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class="board-row mt-2">
+            <div class="board-card">
+                <div class="board-card-header">
+                    <span>Marketing</span>
+                    <span class="progress-bar">
+                        <progress class="progress" max="100" value="30"></progress><span>30%</span>
+                    </span>
+                    
+                </div>
+    
+                <div class="board-card-content-tags mb-1">
+                    <span class="kanban-tag tag-r">Developpment</span>
+                    <span class="kanban-tag tag-y">Research</span>
+                    <span class="kanban-tag">Planing</span>
+                </div>
+    
+                <div class="board-card-footer">
+                    <div class="board-card-items">
+                        <span class="board-card-item"><i class="fa-solid fa-table-list"></i> 51</span>
+                        <span class="board-card-item"><i class="fa-brands fa-trello"></i> 25</span>
+                        <span class="board-card-item"><i class="fa-solid fa-bug"></i> 12</span>
+                        <span class="board-card-item"><i class="fa-solid fa-mug-hot"></i> 100+</span>
+                    </div>
+                    <div class="kanban-users-strack">
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault2.jpg') }}');"></div>
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault9.jpg') }}');"></div>
+                        <div class="a-round-img-xs" style="background-image:url('{{ asset('img/userDefault.jpg') }}');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
+
